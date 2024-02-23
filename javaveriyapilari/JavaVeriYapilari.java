d4e2dcfa5c0113284aa7af252735bb11947d6a04
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2024-02-23 17:58:38 +0300

    java-beginner

---
+++ javaveriyapilari/JavaVeriYapilari.java
@@ -0,0 +1,37 @@
+package javaveriyapilari;
+
+import java.util.Scanner;
+
+public class JavaVeriYapilari {
+
+    public static void main(String[] args) {
+        
+        boolean result;
+        String isim1="Ahmet";
+        String isim2="Ahmet";
+        result = isim1==isim2;
+        System.out.println(result);
+        result = isim1=="Ahmet";
+        System.out.println(result);
+        
+//        Scanner scan = new Scanner(System.in);
+//        int sayi;
+//        String metin;
+//        int [] dizi1 = new int [5];
+//        dizi1[0] = 5;
+//        int [] dizi2 = {18,21,39};
+//        
+//        System.out.print("Hello Java!\n");
+//        System.out.println(dizi1[0]);
+//        System.out.println(dizi2[1]);
+//        System.out.print("Sayi girin : ");
+//        sayi = scan.nextInt();
+//        System.out.println("Girdiginiz sayi = "+sayi);
+//        scan.nextLine();
+//        System.out.print("Metin girin : ");
+//        metin = scan.nextLine();
+//        System.out.println("Girdiginiz metin = "+metin);
+        
+    }
+    
+}
